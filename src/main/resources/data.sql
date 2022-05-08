6e0bf7e81813d2770132b8041d66b31db0abaca4
set404 <89992008819@mail.ru>
2022-05-09 01:10:10 +0300

    - Add global sequence to BD  - Refactoring BD and Model (Delete Menu)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,38 +1,33 @@
-INSERT INTO USERS (email, name, password, registered)
-VALUES ('user@ya.ru', 'User', '{noop}password', DATEADD('DAY', -10, CURDATE())),
-       ('admin@ya.ru', 'Admin', '{noop}password', DATEADD('DAY', -15, CURDATE()));
+DELETE FROM USERS;
+DELETE FROM USER_ROLES;
+DELETE FROM RESTAURANT;
+DELETE FROM DISH;
+DELETE FROM VOTE;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+
+INSERT INTO USERS (email, name, password)
+VALUES ('user@ya.ru', 'User', '{noop}password'),
+       ('admin@ya.ru', 'Admin', '{noop}password');
 
 INSERT INTO USER_ROLES (role, user_id)
-VALUES ('USER', 1),
-       ('ADMIN', 2),
-       ('USER', 2);
+VALUES ('USER', 100000),
+       ('USER', 100001),
+       ('ADMIN', 100001);
 
 INSERT INTO RESTAURANT (name)
 VALUES ('Ollis'),
        ('2berega'),
        ('larisuvannuhochu');
 
-
 INSERT INTO DISH (name, price, restaurant_id)
-VALUES ('Borsh', 200, 1),
-       ('Pizza', 700, 1),
-       ('Sushi', 500, 2),
-       ('Burger', 600, 2),
-       ('Shawerma', 150, 3),
-       ('Potatoes Free', 100, 3);
-
-INSERT INTO MENU (name, date, restaurant_id)
-VALUES ('Ollis 1', CURRENT_DATE, 1),
-       ('Ollis 2', CURRENT_DATE, 1),
-       ('2berega 1', CURRENT_DATE, 2),
-       ('2berega 2', CURRENT_DATE, 2),
-       ('larisuvannuhochu', CURRENT_DATE, 3),
-       ('larisuvannuhochu', CURRENT_DATE, 3);
+VALUES ('Borsh', 200, 100002),
+       ('Pizza', 700, 100002),
+       ('Sushi', 500, 100003),
+       ('Burger', 600, 100003),
+       ('Shawerma', 150, 100004),
+       ('Potatoes Free', 100, 100004);
 
 INSERT INTO VOTE (date, user_id, restaurant_id)
-VALUES (CURDATE(), 1, 2),
-       (DATEADD('DAY', -1, CURDATE()), 1, 3),
-       (DATEADD('DAY', -2, CURDATE()), 1, 1),
-       (CURDATE(), 2, 3),
-       (DATEADD('DAY', -1, CURDATE()), 2, 1),
-       (DATEADD('DAY', -2, CURDATE()), 2, 2);
+VALUES (now(), 100000, 100002),
+       (now(), 100001, 100003);
